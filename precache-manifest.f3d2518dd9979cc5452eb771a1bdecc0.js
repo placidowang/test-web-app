d015d0fd5e6db2d7562c7cc59d029536dbc3a0ed
placidowang <placidow@gmail.com>
2020-08-14 16:54:06 -0500

    Updates

---
+++ precache-manifest.f3d2518dd9979cc5452eb771a1bdecc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b55429849f8da7fe2ce6e065cc7392a3",
+    "url": "/test-web-app/index.html"
+  },
+  {
+    "revision": "1e08b90f9ab8b07ff932",
+    "url": "/test-web-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fbf5ff32327e2d710d3a",
+    "url": "/test-web-app/static/js/2.b5ee70cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-web-app/static/js/2.b5ee70cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e08b90f9ab8b07ff932",
+    "url": "/test-web-app/static/js/main.610d49c1.chunk.js"
+  },
+  {
+    "revision": "618ee7c1bcfc156378b0",
+    "url": "/test-web-app/static/js/runtime-main.1ce04059.js"
+  }
+]);
