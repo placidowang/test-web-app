7d19b038f72f2a6d5873be90267df5674405d0ba
placidowang <placidow@gmail.com>
2020-08-14 17:15:27 -0500

    testing change

--- src/App.js
+++ src/App.js
@@ -10,14 +10,6 @@ function App() {
         <p>
           ding dong
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
