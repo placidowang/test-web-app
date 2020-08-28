4c22cbc3f463cbf14ee43a096729c6c188d38299
placidowang <placidow@gmail.com>
2020-08-27 22:57:02 -0500

    video responsive to desktop and mobile

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <p>
           Nothing to see here. Here's a silly video instead.
         </p>
-        <div style={{height: '60vw', width: '90vw'}}>
+        <div className='vidWrap'>
           {/* eslint-disable-next-line */}
           <iframe width="100%" height="100%" src="https://www.youtube.com/embed/j6LjqDd_QcU" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
