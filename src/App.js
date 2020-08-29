ecb08c5abf72aa4192494a0c1d28b5f6878da691
placidowang <placidow@gmail.com>
2020-08-28 20:54:11 -0500

    cidowang

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import pika from './pikachu-lets-go.png';
 import './App.css';
+import Navbar from './Navbar.js';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Navbar />
+      <main className="App-main">
         <img src={pika} className="App-logo" alt="logo" />
         <h1>
           hi
         </h1>
         <p>
-          Nothing to see here. Here's a silly video instead.
-        </p>
-        <p>
-          Also Jonathan's a CAC
+          Website coming soon. In the meantime, here's a silly video.
         </p>
         <div className='vidWrap'>
           {/* eslint-disable-next-line */}
           <iframe width="100%" height="100%" src="https://www.youtube.com/embed/j6LjqDd_QcU" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
-      </header>
+      </main>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => <div className='navbar'>
+  <h1>cidowang</h1>
+</div>
+
+export default Navbar;
