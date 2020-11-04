8a58bb4a9bdecb13277f5bbfce882b3a48ece6c1
Placido Wang <placidow@gmail.com>
2020-11-04 00:27:59 -0600

    testing git push from ubuntu laptop/ssh

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
         <Route exact path='/' component={Home} />
         <Route exact path='/about' component={About} />
         <Route exact path='/contact' component={Contact} />
+        <div value='hello?'/>
       </div>
     </Router>
   );
